d75d4e0f0384b1bf7b81cd1f3c98f4502448686d
Johannes <s8jsweis@stud.uni-saarland.de>
2020-06-19 17:16:05 +0200

    added Meto's changes and rewrote test bench (not sure if correctly, though)

--- skeleton/aufwaermteil/MealyMaschine.v
+++ skeleton/aufwaermteil/MealyMaschine.v
@@ -1,60 +1,69 @@
 module MealyPattern(
-	input        clock,
-	input        i,
-	output [1:0] o
+    input        clock,
+    input        i,
+    output [1:0] o
 );
-	reg [2:0] q = 3'b000;
-	always @(posedge clock)
-		begin
-			q = q << 1;
-			q[0] = i;
-			if((q == 3'b111) | (q == 3'b001))
-				o = 2'b10;
-			else
-				o = 2'b00;
-		end
-
+    reg [2:0] q = 3'b000;
+    reg [1:0] out = 2'b00;
+    always @(posedge clock)
+        begin
+            q = q << 1;
+            q[0] = i;
+            if((q == 3'b111) | (q == 3'b001))
+                out <= 2'b10;
+            else
+                out <= 2'b00;
+        end
+    
+    assign o = out;
+    
 endmodule
 
 
-
 module MealyPatternTestbench();
-
-    reg clk, in, tmp;
+    
+    //10b'1110011001
+    reg clk, in;
+    
     
     initial
     begin
-        $dumpfile("MealyMaschine.vcd");
+        $dumpfile("MealyPattern.vcd");
         $dumpvars;
-        
-        assign in = 10'b1110011001
-        
-        // part of in is written into tmp for easier validation
-        // and update in accordingly
-        tmp <= in [9:7];
-        in <= in << 3;
-        
-        
-        // check if output was as expected
-        if (((tmp == 3'b111) | (tmp == 3'b001)) & (out == 2'b01))
-            $display ("CORRECT: input was %h and output was %h", tmp, out);
-        else
-            $display ("WRONG: input was %h and output was %h", tmp, out);
+    
+        in <= 1'b1;
+        #1;
+        in <= 1'b1;
+        #1;
+        in <= 1'b1;
+        #1;
+        in <= 1'b0;
+        #1;
+        in <= 1'b0;
+        #1;
+        in <= 1'b1;
+        #1;
+        in <= 1'b1;
+        #1;
+        in <= 1'b0;
+        #1;
+        in <= 1'b0;
+        #1;
+        in <= 1'b1;
+        #1;
         
         $finish;
     end
     
     
-    // input tmp for machine
     wire [1:0] out;
-    MealyPattern machine(.clock(clk), .i(tmp), .o(out));
-        
-
-    // create clock
+    MealyPattern machine(.clock(clk), .i(in), .o(out));
+    
+    
+    
     always
     begin
-        clk <= 1'b1; #1; clk <= 1'b0; #1;
+        clk <= 1'b0; #1; clk <= 1'b1; #1;
     end
 
 endmodule
-
