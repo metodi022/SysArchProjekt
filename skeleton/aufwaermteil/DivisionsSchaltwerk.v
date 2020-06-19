a948f2f1b73de81d4427e2b9a305d9c33094aea0
Johannes <s8jsweis@stud.uni-saarland.de>
2020-06-19 19:02:10 +0200

    added skeleton structure for DivisionSchaltwerk.v

--- skeleton/aufwaermteil/DivisionsSchaltwerk.v
+++ skeleton/aufwaermteil/DivisionsSchaltwerk.v
@@ -6,8 +6,50 @@ module Division(
 	output [31:0] q,
 	output [31:0] r
 );
-
-	// TODO Implementierung
-
+    
+    reg count;
+    reg [31:0] currentR;        // first register
+    reg [31:0] currentB;        // second register
+    reg [31:0] doneRemaining    // speichert noch benötigte Bits v. Dividend A und bereits berechnete
+                                // Bits v. Quotient Q  =>  {Q[N − 1 : i + 1], A[i : 0]} vor jeder Iteration
+    
+    always @(posedge clock)
+    begin
+        if (start == 1)
+        begin
+            assign count <= 0;
+            assign currentR <= 1'b0;
+            assign currentB <= b;
+            assign doneRemaining <= 32'b00000000000000000000000000000000;
+        end
+        
+        
+        if (count != 32)
+        begin
+            
+            // TODO Implementierung
+            /*  R = 0
+                for i = N-1 to 0
+                    R’ = 2 * R + A[i]
+                    if (R’ < B) then Q[i] = 0, R = R’
+                    else Q[i] = 1, R = R’-B
+            */
+            
+        end
+           
+           
+	
+	
+	
+	
+	
+	
+	
+	always
+	begin
+        clock <= 1'b1; #1; clock <= 1'b0; #1;
+    end
+	
+	
 endmodule
 
