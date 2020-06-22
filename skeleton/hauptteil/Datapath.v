25af2811991cd5438654fefcb1cfe1e5e9519a30
Angelika Wolf <s8anwolf@stud.uni-saarland.de>
2020-06-22 10:19:21 +0200

    ALU

--- skeleton/hauptteil/Datapath.v
+++ skeleton/hauptteil/Datapath.v
@@ -116,6 +116,30 @@ module ArithmeticLogicUnit(
 	output        zero
 );
 
-	// TODO Implementierung der ALU
+	always @(a or b or alucontrol)
+    begin
+      case (alucontrol)
+        3'b000:
+          result = = (a < b ? 1 : 0);
+        
+        3'b001:
+          result = a - b;
+        
+        3'b101:
+          result = a + b;
+        
+        3'b110:
+          result = a | b;
+        
+        3'b111:
+          result = a & b;
+      endcase
+      
+      if (result == 0)
+      zero = 1;
+      else
+      zero = 0;
+      
+    end
 
 endmodule
